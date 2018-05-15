4b686cb452eaed22b32a00c7900739f64cacb861
duytrung1394 <trung1394@gmail.com>
2018-05-15 10:34:41 +0700

    added config.php file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    define('SQL_HOST','mysql');
+    define('DB_HOST','sql9.freemysqlhosting.net');
+    define('DB_NAME','sql9238008');
+    define('DB_USER_NAME','sql9238008');
+    define('DB_PASSWORD','qKpYXVmkCl');
+    define('POST_NUMBER','3306');
+?>
--- library/connect.php
+++ library/connect.php
@@ -1,7 +1,8 @@
 <?php
-	$db="mysql:host=localhost;dbname=webtintuc";
-	$user="root";
-	$pass="";
+	include_once('../config.php');
+	$db="mysql:host=".DB_HOST.";dbname=".DB_NAME;
+	$user= DB_USER_NAME;
+	$pass= DB_PASSWORD;
 	 $option=array(
 	 PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
 	 PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION  //PDO ném ra ngoại lệ khi gặp lỗi đồng thời tạo ra PHP warning
