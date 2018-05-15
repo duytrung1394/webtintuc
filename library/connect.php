08a8b1b56cca1650db9a74e9bfde9571bd9a813a
duytrung1394 <trung1394@gmail.com>
2018-05-15 10:43:23 +0700

    test config

--- library/connect.php
+++ library/connect.php
@@ -1,8 +1,7 @@
 <?php
-	include_once('../config.php');
-	$db="mysql:host=".DB_HOST.";dbname=".DB_NAME;
-	$user= DB_USER_NAME;
-	$pass= DB_PASSWORD;
+	$db="mysql:host=sql9.freemysqlhosting.net;dbname=sql9238008";
+	$user="sql9238008";
+	$pass="qKpYXVmkCl";
 	 $option=array(
 	 PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
 	 PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION  //PDO ném ra ngoại lệ khi gặp lỗi đồng thời tạo ra PHP warning
